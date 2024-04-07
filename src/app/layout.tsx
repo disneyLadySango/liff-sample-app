71befbae848e9cfe2a370e95f3a4d0a04b06b710
Wayama <42468349+disneyLadySango@users.noreply.github.com>
2024-04-07 21:26:56 +0900

    [環境構築]EsLintでのimportのsort設定 (#2)
    * chore: lint-importsの追加
    
    * feat: lintの設定追加
    
    * chore: settings.jsonを追加
    
    * lint: format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import { Providers } from "./providers";
+
+import type { Metadata } from "next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
