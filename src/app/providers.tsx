773df6a35478040debbbf66fb0917f63fc1d5c74
disneyLadySango <sangota.d.duck@gmail.com>
2024-04-07 21:25:49 +0900

    lint: format

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
-import { ChakraProvider } from "@/components/design-system";
 import { FC, ReactNode } from "react";
+
+import { ChakraProvider } from "@/components/design-system";
 
 export const Providers: FC<{ children: ReactNode }> = ({ children }) => {
   return <ChakraProvider>{children}</ChakraProvider>;
