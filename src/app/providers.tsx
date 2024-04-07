bad7a2496340017752e70354f2d83fb1e0524d5f
Wayama <42468349+disneyLadySango@users.noreply.github.com>
2024-04-07 22:38:41 +0900

    [環境構築]Liffの設定を追加 (#5)
    * chore: add liff
    
    * feat: 設定周りを追加

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,13 @@
 import { FC, ReactNode } from 'react'
 
+import { LineProvider } from '@/features/line/Provider'
+
 import { ChakraProvider } from '@/components/design-system'
 
 export const Providers: FC<{ children: ReactNode }> = ({ children }) => {
-  return <ChakraProvider>{children}</ChakraProvider>
+  return (
+    <ChakraProvider>
+      <LineProvider>{children}</LineProvider>
+    </ChakraProvider>
+  )
 }
---
+++ src/features/line/Provider/index.ts
@@ -0,0 +1,1 @@
+export { LineProvider, useLine } from './Core'
