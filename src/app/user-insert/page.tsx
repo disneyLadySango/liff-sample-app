cb6f6913e0cf10d4a07734eb4199854e846e1e07
disneyLadySango <sangota.d.duck@gmail.com>
2024-09-05 22:31:41 +0900

    chore: debug

--- src/app/user-insert/page.tsx
+++ src/app/user-insert/page.tsx
@@ -15,36 +15,42 @@ const UserInsertPage: FC = () => {
 
   const auth = useAuth0()
 
-  const onInsert = () => {
+  const onInsert = async () => {
     const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string
     const apiKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY as string
 
-    const call = async () => {
-      const profile = await liff?.getProfile()
-      const userId = auth.user?.sub
-      const accessToken = await auth.getAccessTokenSilently()
-      const body = {
-        line_id: profile?.userId,
-        id_token: userId,
-      }
-      setRequestBody(JSON.stringify({ ...body, accessToken }))
-      const result = await fetch(`${url}/rest/v1/users`, {
-        headers: {
-          apikey: apiKey,
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
-        },
-        method: 'POST',
-        body: JSON.stringify(body),
-      })
-      if (!result.ok) {
-        console.error('Failed to insert user:', result)
-        alert('ユーザー登録に失敗しました')
-        return
-      }
-    }
+    alert('登録処理を開始します')
 
-    void call()
+    const profile = await liff?.getProfile()
+
+    alert(`プロフィール取得完了: ${profile?.userId}`)
+
+    const userId = auth.user?.sub
+
+    const accessToken = await auth.getAccessTokenSilently()
+
+    alert(`アクセストークン取得完了: ${accessToken}`)
+
+    const body = {
+      line_id: profile?.userId,
+      id_token: userId,
+    }
+    setRequestBody(JSON.stringify({ ...body, accessToken }))
+    const result = await fetch(`${url}/rest/v1/users`, {
+      headers: {
+        apikey: apiKey,
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      method: 'POST',
+      body: JSON.stringify(body),
+    })
+    if (!result.ok) {
+      console.error('Failed to insert user:', result)
+      alert('ユーザー登録に失敗しました')
+      return
+    }
+    alert('ユーザー登録に成功しました')
   }
 
   return (
